79e6510a7f89b1e679166b1a2bc1f12a01db912a
Chuka Okoye <chukanorth@hotmail.co.uk>
2021-06-09 15:17:38 +0100

    Added rspec and root files

---
+++ app.rb
@@ -0,0 +1,14 @@
+require 'sinatra/base'
+require 'sinatra/reloader'
+
+class Battle < Sinatra::Base
+  configure :development do
+    register Sinatra::Reloader
+  end
+
+  get '/' do
+    'Hello Battle!'
+  end
+  # start the server of ruby file executed directly
+  run! if app_file == $0
+end
